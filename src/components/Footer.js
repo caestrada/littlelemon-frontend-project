8b79663fb87e49c7b498ef7bd774829f9f57fb17
Carlos Estrada <cestr008@gmail.com>
2023-10-24 13:11:41 -0400

    update nav and footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,6 +2,26 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <footer>Footer</footer>
+    <footer>
+      <img src="/logo.svg" alt="logo" />
+      <div>
+        <h2>Doormat Navigation</h2>
+        <p>Home</p>
+        <p>About</p>
+        <p>Menu</p>
+      </div>
+      <div>
+        <h2>Contact</h2>
+        <p>Address</p>
+        <p>Phone Number</p>
+        <p>Email</p>
+      </div>
+      <div>
+        <h2>Social Media Links</h2>
+        <p>Address</p>
+        <p>Phone Number</p>
+        <p>Email</p>
+      </div>
+    </footer>
   )
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,6 +2,15 @@ import React from 'react'
 
 export default function Nav() {
   return (
-    <nav>Nav</nav>
+    <nav>
+        <img src="/logo.svg" alt="logo" />
+        <ul>
+            <li><a href='/'>Home</a></li>
+            <li><a href='/about'>About</a></li>
+            <li><a href='/reservations'>Reservations</a></li>
+            <li><a href='/order-online'>Order Online</a></li>
+            <li><a href='/login'>Login</a></li>
+        </ul>
+    </nav>
   )
 }
