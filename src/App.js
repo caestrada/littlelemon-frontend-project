87a2c1e696333fddd98413a21d0466ce1710f80c
Carlos Estrada <cestr008@gmail.com>
2023-10-24 19:25:27 -0400

    hero section

--- src/App.js
+++ src/App.js
@@ -7,7 +7,16 @@ function App() {
     <>
       <Header />
       <main>
-        Homepage
+        <section className='hero'>
+          <div className="content">
+            <h1>Little Lemon</h1>
+            <h2>Chicago</h2>
+            <p>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p>
+            <button>Reserve a Table</button>
+          </div>
+
+          <img src="/images/restauranfood.jpg" alt="logo" className="hero-image" />
+        </section>
       </main>
       <Footer />
     </>
